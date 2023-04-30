a7dbab7cda35fe41e96e6344089c680cce4f03a0
mcgue <mcgu@protonmail.com>
2023-04-30 13:46:54 -0700

    add explanation and input

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # Calculate whether a year is a leap year
 
-def
-
+def explanation():
+    # Print what the program is about
+    print("I can determine if any year is a leap year")
 
 
 if __name__ == '__main__':
-
+    # Introduce program
+    explanation()
+    # Get input
+    year = input("Enter the year would you like to check: ")

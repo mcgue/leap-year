d11d97bbd64af4e01a155558dac31bf0edc5f3c9
mcgue <mcgu@protonmail.com>
2023-05-03 09:57:40 -0700

    add basics of function to calculate leap year and print out result in Main

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ def explanation():
     # Print what the program is about
     print("I can determine if any year is a leap year")
 
+def calculate(year):
+    # Divide by 4
+    if year % 4 == 0:
+        return "This is a leap year"
+    else:
+        return "probably not"
+
 
 if __name__ == '__main__':
     # Introduce program
     explanation()
     # Get input
-    year = input("Enter the year would you like to check: ")
+    year = int(input("Enter the year would you like to check: "))
+    print(calculate(year))

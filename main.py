a6a7ac9b6fb2c07e6787eeea3cb7692438b16c2b
mcgue <mcgu@protonmail.com>
2023-05-12 10:23:17 -0700

    finish function to determine whether leap year

--- main.py
+++ main.py
@@ -10,22 +10,19 @@ def calculate(year):
         return "Leap years had not started yet. Caesar implemented \n" \
                "them in 45 bce."
     # Divide by 4
-    if year % 4 != 0:
+    elif year % 4 != 0:
         return "This is not a leap year"
-    if year >= 1582:
+    elif year >= 1582:
         if (year % 4 == 0 and year % 400 == 0):
-            return "This is a leap year with an extra day on February 29th"
+            return "For those places using the Gregorian \n" \
+                    "calendar, this is/was a leap year with \n" \
+                    "an extra day on February 29th"
         else:
             return "This is not a leap year"
-        #elif (year % 400 != 0) or
-    elif year > -46:
-        return "This was a year during the Julian Calendar.\n" \
-               "At the time, leap day was February 24.\n" \
-               "February was the last month of the year."
     else:
-        return "huh?"
-
-
+        return "This was a leap year during the Julian Calendar.\n" \
+               "At the time, leap day was an extra February 24th\n" \
+               "and February was the last month of the year."
 
 if __name__ == '__main__':
     # Introduce program

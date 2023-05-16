28c1673272bc8928aab9adf59d247b0fc0c01ecf
mcgue <mcgu@protonmail.com>
2023-05-16 14:52:43 -0700

    add comments, try to fix verb indicating past/current/future

--- main.py
+++ main.py
@@ -4,28 +4,34 @@ from datetime import datetime
 
 # Function to calculate whether a leap year
 def calculate(year):
-    # Determine is past or future
+    # Determine current year
     today = datetime.now().year
-    # Leap years not invented yet
+    # if current year
+    if year == today:
+        verb = 'is'
+    # If in the future
+    elif year > today:
+        verb = 'will be'
+    # If the past
+    else:
+        verb = 'was'
+    # If before leap year invention
     if year < -45:
         return "Leap years had not started yet. Caesar implemented \n" \
                "them in 45 bce."
-    # Divide by 4
+    # If not divisible by 4
     elif year % 4 != 0:
-        return "This is not a leap year"
+        return f"This {verb} not a leap year"
+    # If after implementation of Gregorian calendar
     elif year >= 1582:
-        if year == today:
-            verb = 'is'
-        elif year > today:
-            verb = 'will be'
-        else:
-            verb = 'was'
+        # Yes to leap year if divisible by 4 and only years divisible by 400
         if (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
             return "For those places using the Gregorian \n" \
                     f"calendar, this {verb} a leap year with \n" \
                     "an extra day on February 29th"
         else:
-            return "This is not a leap year"
+            return f"This {verb} not a leap year"
+    # Remaining are leap years in Julian Calendar
     else:
         return "This was a leap year during the Julian Calendar.\n" \
                "At the time, leap day was an extra February 24th\n" \
@@ -33,7 +39,7 @@ def calculate(year):
 
 if __name__ == '__main__':
     # Introduce program
-    print("I can determine if any year is a leap year")
+    print("I can determine if any year is a leap year. Enter a negative if BCE")
     # Get input
     year = int(input("Enter the year would you like to check: "))
     # Run function to determine whether year is a leap year
